6c91319dea6e2124eefc0e48f358aac66219be39
Aswinvaasits <84697530+Aswinvaasits@users.noreply.github.com>
2022-08-15 22:17:04 +0530

    updtejs

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ close.addEventListener('click',function(e){
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("navbar-header").style.top = "0";
   } else {
     document.getElementById("navbar-header").style.top = "-250px";
