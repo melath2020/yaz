b8366a5ff92c0f55d425f1a53a6908ccb7560cd9
Aswinvaasits <84697530+Aswinvaasits@users.noreply.github.com>
2022-08-15 19:33:15 +0530

    update

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ close.addEventListener('click',function(e){
 })
 
 
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
+
+
 
 
 
